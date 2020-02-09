77524c8176c1e4729b236dd6d7513f637823ccea
sergevitali <srp_yldrm@yahoo.co.uk>
2020-02-09 21:42:04 +1100

    Updates

---
+++ precache-manifest.59ff9b29d8f0ca46e39a5abed8cd16f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fbe1939dafa745099057e322eb74625",
+    "url": "/address-book-react-app/index.html"
+  },
+  {
+    "revision": "08e6c2ccc628a7b1c348",
+    "url": "/address-book-react-app/static/css/main.d29d5d5c.chunk.css"
+  },
+  {
+    "revision": "23061e65fa96861691a0",
+    "url": "/address-book-react-app/static/js/2.aa62f1cd.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/address-book-react-app/static/js/2.aa62f1cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08e6c2ccc628a7b1c348",
+    "url": "/address-book-react-app/static/js/main.cdd3b24f.chunk.js"
+  },
+  {
+    "revision": "d133f3d9a227ed6258f2",
+    "url": "/address-book-react-app/static/js/runtime-main.fe1d8895.js"
+  }
+]);
